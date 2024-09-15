1123305d1271b2341365100359013e201bb62dc9
ujw0704 <ujwalsharma571@gmail.com>
2024-09-15 23:07:25 +0530

    few

--- src/Components/Productcart.js
+++ src/Components/Productcart.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Productcart() {
+  let [cart, setCart] = useState()
+
+
+
+  
   return (
-    <div></div>
+    <div>
+{
+  cart.map((items, index)=>{
+    return(
+
+      <div key={items.name}>
+<img src={items.image} alt={items.name} />
+<h6>{items.description}</h6>
+<h2> RS :{items.price}</h2>
+    </div>
+    )
+
+  })
+}
+
+
+    </div>
   )
 }
 
